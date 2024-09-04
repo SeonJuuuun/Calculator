fb6103ea10b7c0e7d15bdbe19d406941cac45fc1
yuseonjun <seonjun0906@gmail.com>
2024-09-04 20:35:09 +0900

    [Feat] Level1 정수형이 아니면 예외 발생 구현

--- src/level1/Main.java
+++ src/level1/Main.java
@@ -1,4 +1,25 @@
 package level1;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Main {
+    private static final String NUMBER_REG = "^[0-9]*$";
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("첫 번째 숫자를 입력해주세요.");
+        final String firstNumber = sc.next();
+        if (!Pattern.matches(NUMBER_REG, firstNumber)) {
+            throw new IllegalArgumentException("양의 정수만 입력이 가능합니다.");
+        }
+
+        System.out.println("두 번째 숫자를 입력해주세요.");
+        final String secondNumber = sc.next();
+        if (!Pattern.matches(NUMBER_REG, secondNumber)) {
+            throw new IllegalArgumentException("양의 정수만 입력이 가능합니다.");
+        }
+        sc.close();
+    }
 }
