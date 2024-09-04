4de578f62ad7aeaace60c754596d3731fdb671b3
yuseonjun <seonjun0906@gmail.com>
2024-09-04 20:36:00 +0900

    [Feat] Level1 사칙연산 기호 입력 및 예외 처리 구현

--- src/level1/Main.java
+++ src/level1/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main {
+    private static final String OPERATION_REG = "[+\\-*/]";
     private static final String NUMBER_REG = "^[0-9]*$";
 
     public static void main(String[] args) {
@@ -19,6 +20,12 @@ public class Main {
         final String secondNumber = sc.next();
         if (!Pattern.matches(NUMBER_REG, secondNumber)) {
             throw new IllegalArgumentException("양의 정수만 입력이 가능합니다.");
+        }
+
+        System.out.println("사칙 연산을 입력해주세요.");
+        final String operator = sc.next();
+        if (!Pattern.matches(OPERATION_REG, operator)) {
+            throw new IllegalArgumentException("사칙 연산중 하나만 입력해주세요.");
         }
         sc.close();
     }
