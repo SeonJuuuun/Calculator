5fc61592d0f02f89f81dbf86e3ac4b44f4bb763a
yuseonjun <seonjun0906@gmail.com>
2024-09-08 18:31:01 +0900

    [Feat] Level3 -와 / 수정

--- src/level3/operation/SubstractOperation.java
+++ src/level3/operation/SubstractOperation.java
@@ -5,9 +5,9 @@ public class SubstractOperation implements Operation<Number> {
     @Override
     public Number operate(final Number firstNumber, final Number secondNumber) {
         if (firstNumber instanceof Double || secondNumber instanceof Double) {
-            return firstNumber.doubleValue() / secondNumber.doubleValue();
+            return firstNumber.doubleValue() - secondNumber.doubleValue();
         } else {
-            return firstNumber.intValue() / secondNumber.intValue();
+            return firstNumber.intValue() - secondNumber.intValue();
         }
     }
 }
